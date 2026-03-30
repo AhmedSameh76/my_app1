72776d99dd1e880fc9726b64d663858b9f6894d9
ahmed <ahmedameh76@gmail.com>
2026-03-30 02:22:22 +0200

    update

---
+++ int.dart
@@ -0,0 +1,8 @@
+void main() {
+  int x = 5;
+  "ali";
+}
+
+class cars {
+  String name = " ijeifh";
+}
---
+++ widdget.dart
@@ -0,0 +1,8 @@
+void main() {
+  double z = 5;
+  "lpkvojj";
+}
+
+class cars {
+  String name = " ahmeddd";
+}
